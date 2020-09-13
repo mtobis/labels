86baa9447b1711730b167bde665651bf0fbc7c82
Michael Tobis <mt@gmail.com>
2020-09-13 14:24:38 -0400

    templates

--- printesg.py
+++ printesg.py
@@ -59,30 +59,21 @@ separator = """<div>
 </div>
 <p>&nbsp;</p> """
 
-blockx = """<div style="border: 2px solid black; border-color: black; padding:20px; margin:40px; text-align:center; overflow:hidden; width:400px; clear=both;" >
-<span style="font-size:x-large; font-family:Lato;">TITLE gggg</span><br/>
-<b>ARTIST ggg</b><br/>
-MEDIUM ggg<br/>
-DIMENSIONS<br/>
-PRICE
-</div>
 
-"""
-
-
-block = """<div style="border: 2px solid black; text-align:center; overflow:hidden; width:400px; clear=both; padding:30px;">
-<span style="font-size:x-large; font-family:Lato; font-weight:800;">TITLE gg<em>gg</em></span><br/>
-<b>ARTIST ggg</b><br/>
-MEDIUM ggg<br/>
-DIMENSIONS<br/>
-PRICE
+block = """<div style="border: 2px solid black; text-align:center; overflow:hidden; width:400px; clear=both; padding:30px; {color} {bordermod} ">
+<span style="font-size:x-large; font-family:Lato; font-weight:800;">{title} gg<em>gg</em></span><br/>
+<b>{artist} ggg</b><br/>
+{info}<br/>
+{price}
 </div>
 """
+
+
 
 
 block2 = block.replace('">',' border-top: 0;">',1)
 
-block3 = """<p>&nbsp;</p><div style="border: 2px solid black; overflow:hidden; width:400px; clear=both; text-alicg:center; color: white; border-bottom: 0; padding:30px;">
+block3 = """<p>&nbsp;</p><div style="border: 2px solid black; overflow:hidden; width:400px; clear=both; text-align:center; color: white; border-bottom: 0; padding:30px;">
 <span style="font-size:x-large; font-family:Lato; ">TITLE gggg</span><br/>
 <b>ARTIST ggg</b><br/>
 MEDIUM ggg<br/>
@@ -91,12 +82,33 @@ PRICE
 </div>
 """
 
+b1dict = {
+    "color": "",
+    "bordermod": "",
+    "title": "Frilly Tulip",
+    "artist": "Michael Tobis",
+    "info": 'alcohol ink on tracing paper, 8.5" x 11 "',
+    "price": '$1'
+    }
+
+b1 = block.format(**b1dict)
+
+b2dict = b1dict.copy()
+b2dict["bordermod"] = ' border-top: 0; '
+
+b2 = block.format(**b2dict)
+
+b3dict = b1dict.copy()
+b3dict["bordermod"] = ' border-bottom: 0; '
+b3dict["color"] = ' color: white; '
+
+b3 = block.format(**b3dict)
 
 outfnam = "test.html"
 
 outf = open(outfnam,"w")
 
-content = block + block3 + block2 + separator +"<p>next page</p>"
+content = b1 + "<p>&nbsp;</p>" + b3 + b2 + separator +"<p>next page</p>"
 
 outtext = template.replace("%CONTENT",content)
 
