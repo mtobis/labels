6d58af25a41b92bd447026a8076c7b4f7205b32b
Michael Tobis <mtobis@gmail.com>
2020-09-13 12:53:22 -0400

    looking better

--- printesg.py
+++ printesg.py
@@ -23,7 +23,7 @@ template = """<html>
 /* Bold */
 @font-face {
    font-family: Lato;
-   src: url(Lato2OFL/Lato-Regular.ttf);
+   src: url(Lato2OFL/Lato-Bold.ttf);
    font-weight: bold;
 }
 
@@ -56,23 +56,47 @@ color: red;
 
 separator = """<div>
     <pdf:nextpage /> 
-</div> """
+</div>
+<p>&nbsp;</p> """
 
-block = """<div style="border-style:solid; border-color: black; padding:20px; margin:40px; text-align:center; overflow:hidden; width:400px; clear=both;" >
+blockx = """<div style="border: 2px solid black; border-color: black; padding:20px; margin:40px; text-align:center; overflow:hidden; width:400px; clear=both;" >
 <span style="font-size:x-large; font-family:Lato;">TITLE gggg</span><br/>
 <b>ARTIST ggg</b><br/>
 MEDIUM ggg<br/>
 DIMENSIONS<br/>
 PRICE
 </div>
-<p>&nbsp;</p>
+
 """
+
+
+block = """<div style="border: 2px solid black; text-align:center; overflow:hidden; width:400px; clear=both;">
+<span style="font-size:x-large; font-family:Lato; font-weight:800;">TITLE gg<em>gg</em></span><br/>
+<b>ARTIST ggg</b><br/>
+MEDIUM ggg<br/>
+DIMENSIONS<br/>
+PRICE
+</div>
+"""
+
+
+block2 = block.replace('">',' border-top: 0;">',1)
+
+block3 = """<p>&nbsp;</p><div style="border-top: 2px solid black; overflow:hidden; width:400px; clear=both; text-alicg:center; color: white">
+<span style="font-size:x-large; font-family:Lato;">TITLE gggg</span><br/>
+<b>ARTIST ggg</b><br/>
+MEDIUM ggg<br/>
+DIMENSIONS<br/>
+PRICE
+</div>
+"""
+
 
 outfnam = "test.html"
 
 outf = open(outfnam,"w")
 
-content = block + block + separator + block
+content = block + block3 + block2 + separator + block
 
 outtext = template.replace("%CONTENT",content)
 
